c4fd0ac34ad73087328db04f677410bc630af846
YigitA <yigit@aybuga.com>
2022-08-03 14:56:57 +0300

    ui fix

--- src/components/BorderCountries.js
+++ src/components/BorderCountries.js
@@ -5,8 +5,8 @@ const BorderCountries = (props) => {
     return (
         props.borders && (
             <div className="mt-10 flex flex-col md:flex-row items-start">
-                <span className="w-full md:w-1/2 flex mr-4 mb-4 w-full text-base font-bold sm:w-auto mb-2">Border Countries:</span>
-                <div className="w-full md:w-1/2 flex flex-wrap gap-3 sm:ml-3 mt-3 sm:mt-0">
+                <span className="flex md:min-w-[140px] mr-4 mb-4 w-full text-base font-bold sm:w-auto mb-2">Border Countries:</span>
+                <div className="flex flex-wrap gap-3 sm:ml-3 mt-3 sm:mt-0">
                     {props.borders.map((country, index) => (
                         <div key={index} className="inline-block min-w-[100px] shadow-lg rounded-sm bg-lightElements dark:bg-metus-100 text-center text-sm py-1 px-2 hover:bg-gray-200 dark:hover:brightness-125 transition-all">
                             {country}
